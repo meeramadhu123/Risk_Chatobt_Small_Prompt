78646c4c9971601750d35ef49ad4d25a0e38425c
Tiju-1996 <tiju.varghese@beinex.com>
2025-05-07 14:30:51 +0530

    Update chatbot_utils.py

--- chatbot_utils.py
+++ chatbot_utils.py
@@ -370,7 +370,7 @@ def generate_sql_query_for_retrieved_tables(selected_docs, user_question, exampl
            - Use appropriate **INNER JOIN, LEFT JOIN, or RIGHT JOIN** when multiple tables are involved.  
            - Always specify the **correct primary and foreign key relationships** from the metadata. 
         8. **Very Important**- In query use the tablename where all informations/columns relevant ot user question is available, in case it is not available, please join with other tablename where this information is available.
-        9. **Fuzzy matching **-Please use LIKE % incase of fuzzy matching with string for filtering purpose when there is a doubt about actual value/condition.
+        9. **Fuzzy matching **-Please use LIKE % incase of fuzzy matching with string for filtering purpose when there is a doubt about actual value/condition. Please prefer LIKE instead of '=' wherever applicable in SQL query.
         10. **Column name**- Show (SELECT) all the required column names/counts/aggregates(sum,max,min,avg etc.) from required table/tables to answer the question correctly.
         11. Please Replace risk_type column with risk_category1 in SQL query if it is there.
         ## User's Question: {question}  
